6e6bf2b677e4c8ad3aa05985b09c84662672f26b
vijaykumar <vijay1111mane@gmail.com>
2022-05-31 18:04:25 +0530

    commit;

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,55 @@
 <template>
-<h1>Call API</h1>
-<p v-for ="item in list" :key="item" >
-{{item.id}} --- {{item.email}} --- {{item.first_name}}
-</p>
+  <h1>User Data</h1>
+  <!-- <h1>Call API</h1>
+        <p v-for ="item in list" :key="item" >
+            {{item.id}} --- {{item.email}} --- {{item.first_name}}
+        </p> -->
+    <ul class="item" v-for="item in list" :key="item.id">
+        <li>{{item.id}}</li>
+        <li>{{item.email}}</li>
+        <li>{{item.first_name}}</li>
+        <li><img :src="item.avatar" /></li>
+
+    </ul>
 </template>
 
 <script>
-import axios from 'axios';
+import axios from "axios";
 export default {
-   name: "App",
-    data()
-    {
-        return{
-            list:[]
-        }
-    },
-    async mounted()
-    {
-        let result = await axios.get("https://reqres.in/api/users?page=1");
-        console.log("Api data",result.data.data);
-        this.list =result.data.data;
-    }
+  name: "App",
+  data() {
+    return {
+      list: [],
+    };
+  },
+  async mounted() {
+    let result = await axios.get("https://reqres.in/api/users?page=1");
+    console.log("Api data", result.data.data);
+    this.list = result.data.data;
+  },
 };
 </script>
 
 <style>
-#app {
+.item{ 
+    display: flex;
+}
+.item img{
+    width:50px;
+}
+.item li{
+    display: inline-block;
+    width: 180px;
+    border: 1px solid;
+    text-align: center;
+    padding:5px;
+}
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  /* text-align: center; */
+  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+} */
 </style>
